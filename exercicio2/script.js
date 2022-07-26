138ba1e48e60f604384dad49fb22f638cddfd95d
nosdliw <108556065+nosdliw@users.noreply.github.com>
2022-07-25 22:46:15 -0300

    exercício 3 respondido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,3 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+    const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
 const resposta = `${minhaString}\n${minhaString+minhaString.length}\n${minhaString.trim()+minhaString.length}\n${minhaString.replace("________", "sticioso")}`
 console.log (resposta)
